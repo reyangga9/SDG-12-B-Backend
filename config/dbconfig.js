cb0d3d83f411b633ff1c7dc13bbe393009771a88
reyangga9 <64768099+reyangga9@users.noreply.github.com>
2023-10-05 16:15:13 +0700

    Update dbconfig.js FIXING DATABASE ISSUES

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -4,10 +4,7 @@ dotenv.config();
 
 export const connectDb = () => {
   const mongoUrl = `mongodb+srv://${process.env.MONGO_NAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER}/${process.env.MONGO_DATABASE}?retryWrites=true&w=majority`;
-  mongoose
-    .connect(mongoUrl)
-    .then(() => {
-      console.log("Connected to db");
-    })
-    .catch((e) => console.log("Failed to connect db" + e));
+  mongoose.connect(mongoUrl, { useNewUrlParser: true }, () => {
+    console.log('connected to DB Mongo Atlas!');
+  });
 };
