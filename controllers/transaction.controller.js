63b44ff208cb409e5f85643b9d634a1db7063d32
reyangga9 <reyangga5@gmail.com>
2023-11-15 00:15:32 +0700

    adding update transaction

--- controllers/transaction.controller.js
+++ controllers/transaction.controller.js
@@ -6,7 +6,7 @@ export const createTransaction = async (req, res) => {
   try {
     // const userId = req.user._id;
     const userId = req.user._id; // Adjust this according to your middleware
-    console.log("useridOKEOEOEKOOE", userId);
+    // console.log("useridOKEOEOEKOOE", userId);
     const { cartItems } = req.body;
 
     // Calculate total amount and create items array
@@ -51,6 +51,43 @@ export const createTransaction = async (req, res) => {
     res.status(201).json({ transaction });
   } catch (error) {
     console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export const getTransactionsByUserId = async (req, res) => {
+  const userId = req.user._id;
+  console.log(userId);
+
+  try {
+    const transactions = await Transaction.find({ userId }).exec();
+
+    res.status(200).json({ transactions });
+  } catch (error) {
+    console.error("Error fetching transactions:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export const updateTransactionByUserId = async (req, res) => {
+  const userId = req.user._id;
+  const transactionId = req.params.transactionId;
+
+  try {
+    // Find and update the specific transaction
+    const updatedTransaction = await Transaction.findByIdAndUpdate(
+      { _id: transactionId, userId },
+      { $set: { isCompleted: true } },
+      { new: true } // This option returns the modified document
+    );
+
+    if (!updatedTransaction) {
+      return res.status(404).json({ error: "Transaction not found" });
+    }
+
+    res.status(200).json({ transaction: updatedTransaction });
+  } catch (error) {
+    console.error("Error updating transaction:", error);
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
--- routes/transaction.route.js
+++ routes/transaction.route.js
@@ -1,11 +1,17 @@
 // routes/index.js
 import express from "express";
-import { createTransaction } from "../controllers/transaction.controller.js";
+import {
+  createTransaction,
+  getTransactionsByUserId,
+  updateTransactionByUserId,
+} from "../controllers/transaction.controller.js";
 import { validateAuth } from "../middlewares/auth.middleware.js";
 const router = express.Router();
 
 // Add your existing routes
 router.post("/add", validateAuth, createTransaction);
+router.get("/transactions/", validateAuth, getTransactionsByUserId);
+router.put("/update/:transactionId", validateAuth, updateTransactionByUserId);
 // Add transaction routes
 
 export default router;
